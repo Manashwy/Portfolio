4a78dd93c47388fa6a31155edaa0996450082b0b
Manashwy <manashwy@gmail.com>
2023-02-12 14:03:56 +0530

    Portfolio upload

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Large Language Model - Predicting Shakespere",
+    subtitle: "Natural Language Processing",
+    description:
+      "Built a simple, yet elegant, bigram LLM with self attention mechanism using Shakespere text as input",
+    image: "./project-1.gif",
+    link: "https://github.com/Manashwy/Karpathy_Shakesphere_Gen/tree/main",
+  },
+  {
+    title: "Spotify RecSys",
+    subtitle: "Recommender system for Spotify.",
+    description:
+      "A test run at recommender systems for spotify API, using metadata of songs since the 1920, found in kaggle dataset. Used both supervised and unsupervised learning approach.",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "LayOffs 23",
+    subtitle: "A descriptive EDA on the recent tech layoffs.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
